c5a4837d106aed2ba76223a35921f3d8e5fdf2be
Advay Patil <advaypatil27@gmail.com>
2022-10-20 17:50:11 -0700

    update database schema

--- backend/game/models.py
+++ backend/game/models.py
@@ -2,13 +2,8 @@ from django.db import models
 
 
 class Game(models.Model):
-    LEAGUE_CHOICES = (
-        ('A', 'ARCL'),
-        ('O', 'Other')
-    )
-
     ball_by_ball = models.CharField(max_length=300)
-    league_type = models.CharField(max_length=1, choices=LEAGUE_CHOICES)
+    league_type = models.CharField(max_length=1)
 
     def __str__(self):
         return f'Game: {self.league_type}'
