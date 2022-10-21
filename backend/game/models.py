cf17348ac04e39458b6f935bd90457508b70ca4b
Advay Patil <advaypatil27@gmail.com>
2022-10-21 16:03:29 -0700

    update Game Model;  delete ARCL model

--- backend/game/models.py
+++ backend/game/models.py
@@ -5,16 +5,10 @@ from django.db.models import JSONField
 class Game(models.Model):
     ball_by_ball = models.CharField(max_length=300)
     league_type = models.CharField(max_length=1)
+    batsmen = JSONField(default=None, null=True)
+    bowlers = JSONField(default=None, null=True)
+    extra_data = JSONField(default=None, null=True)
 
     def __str__(self):
         return f'Game #{self.id}: {self.league_type}'
 
-
-class ARCL(models.Model):
-    league_type = models.CharField(max_length=1)
-    batsmen = JSONField()
-    bowlers = JSONField()
-    extra_data = JSONField()
-
-    def __str__(self):
-        return f'ARCL Game #{self.id}: {self.league_type}'
