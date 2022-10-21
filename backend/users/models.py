b385195099bbb6efbdc67199c57195dacd6b4bad
Advay Patil <advaypatil27@gmail.com>
2022-10-21 16:19:23 -0700

    create profile model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    runs = models.IntegerField()
+    matches = models.IntegerField()
+    innings = models.IntegerField()
+    batting_innings = models.IntegerField()
+    bowling_innings = models.IntegerField()
+    wickets = models.IntegerField()
+    overs = models.IntegerField()
+    bowling_runs = models.IntegerField()
+
