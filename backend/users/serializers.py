dbe3e63a2c23427a8993a117291edbab4fc1319b
Advay Patil <advaypatil27@gmail.com>
2022-10-21 16:37:35 -0700

    writecustom serializer for Profile

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.db.migrations.serializer import BaseSerializer
+from django.db.migrations.writer import MigrationWriter
+from .models import Profile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -9,3 +12,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class ProfileSerializer(BaseSerializer):
+    def serialize(self):
+        return repr(self.value), {}
+
+
+MigrationWriter.register_serializer(Profile, ProfileSerializer)
