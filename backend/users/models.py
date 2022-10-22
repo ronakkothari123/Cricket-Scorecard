de6ffa5343575cb4c2502644dd381abe4a54924e
Advay Patil <advaypatil27@gmail.com>
2022-10-21 18:33:17 -0700

    add signal to Profile for easier user creation

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -19,3 +21,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.runs} ({self.balls})'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance: User, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance: User, **kwargs):
+    instance.profile.save()
